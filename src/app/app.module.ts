777676280c94cda3b784c73ddf06406b2e4c791d
SangBV1 <vansangvn996@gmail.com>
2018-07-22 11:32:46 +0700

    [Add] Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TaskService } from './core/services/task.service';
 import { ListTaskComponent } from './components/task/list-task/list-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatSortModule, MatTableModule, MatMenuModule, MatProgressSpinnerModule } from '@angular/material';
+import { LayoutModule } from '../../node_modules/@angular/cdk/layout';
+import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,24 @@ import { ListTaskComponent } from './components/task/list-task/list-task.compone
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule
   ],
   providers: [
     TaskService
--- src/app/components/task/list-task/list-task.component.ts
+++ src/app/components/task/list-task/list-task.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { TaskService } from '../../../core/services/task.service';
 import { TaskViewModel } from '../../../Models/task.model';
+import { MatTableDataSource, MatSort } from '../../../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-list-task',
@@ -10,21 +11,26 @@ import { TaskViewModel } from '../../../Models/task.model';
   styleUrls: ['./list-task.component.css']
 })
 export class ListTaskComponent implements OnInit {
+  listTask: TaskViewModel[];
+  displayedColumns: string[] = ['name', 'description', 'startDateAt', 'finishDateAt'];
+  dataSource = new MatTableDataSource([]);
 
-  listTask : TaskViewModel[];
-  
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(public http: HttpClient,
     private taskService: TaskService
   ) { }
 
   ngOnInit() {
     this.getAllTask();
+    this.dataSource.sort = this.sort;
   }
-  
-  getAllTask(): void{
+
+  getAllTask(): void {
     this.taskService.getAllTask().subscribe(
       dataResponse => {
         this.listTask = dataResponse;
+        this.dataSource = new MatTableDataSource(this.listTask)
       }
     );
   }
